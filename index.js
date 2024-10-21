55d03f29454607d06a56255173e33152a6443f1e
DEEP BHATTACHARJEE <deepasn2001@gmail.com>
2024-10-21 03:53:54 -0700

    updated

--- index.js
+++ index.js
@@ -80,3 +80,34 @@ menuCategories.forEach(category => {
 filterMenu("starter"); // Default to showing starters
 
 
+
+
+
+
+
+
+// Open Modal and Show Image
+function openModal(imageSrc) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+
+    modal.style.display = "block";
+    modalImage.src = imageSrc;
+}
+
+// Close Modal
+function closeModal() {
+    const modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
+
+// Close modal when clicking outside the image
+window.onclick = function(event) {
+    const modal = document.getElementById("imageModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
+
+
